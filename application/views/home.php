70e946f1fabec22ddfbced2f4f6ae2df8b2d9f70
IyonPriyono02 <iyonpriyono02@gmail.com>
2019-04-01 20:52:39 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,89 @@
+	<div id="booking" class="section" style="background-image: url(<?php echo base_url('assets/Background/keretaapi.jpg')?>);">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="booking-cta">
+							<h1>PEMESANAN TIKET</h1>
+						</div>
+					</div>
+					<div class="col-md-7 col-md-offset-1">
+						<div class="booking-form">
+							<form>
+								<div class="row">
+
+									<div class="col-md-7">
+										<div class="form-group">
+											<span class="form-label">Stasiun Asal</span>
+											<select class="form-control">
+												<option>Economy class</option>
+												<option>Business class</option>
+												<option>First class</option>
+											</select>
+											<span class="select-arrow"></span>
+										</div>
+									</div>
+
+
+
+									<div class="col-md-7">
+										<div class="form-group">
+											<span class="form-label">Stasiun Tujuan</span>
+											<select class="form-control">
+												<option>Economy class</option>
+												<option>Business class</option>
+												<option>First class</option>
+											</select>
+											<span class="select-arrow"></span>
+										</div>
+									</div>
+
+								</div>
+								<div class="row">
+									<div class="col-md-7">
+										<div class="form-group">
+											<span class="form-label">Tanggal Berangkat</span>
+											<input class="form-control" type="date" required>
+										</div>
+									</div>
+									
+								</div>
+								<div class="row">
+									<div class="col-md-7">
+										<div class="form-group">
+											<span class="form-label">1 Dewasa</span>
+											<select class="form-control">
+												<option>1</option>
+												<option>2</option>
+												<option>3</option>
+												<option>4</option>
+											</select>
+											<span class="select-arrow"></span>
+										</div>
+									</div>
+									<div class="col-md-7">
+										<div class="form-group">
+											<span class="form-label">0 Infant</span>
+											<select class="form-control">
+												<option>0</option>
+												<option>1</option>
+												<option>2</option>
+												<option>4</option>
+											</select>
+											<span class="select-arrow"></span>
+										</div>
+									</div>
+								</div>
+								<div class="form-btn">
+									<button class="submit-btn">Pesan & Cari Kereta</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
