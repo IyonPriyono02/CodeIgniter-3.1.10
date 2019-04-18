c9834bb2053c0d161b81f1aa0107efc5a06b0ff4
jofardho <46946367+jofardho@users.noreply.github.com>
2019-04-18 09:26:40 +0700

    Add files via upload

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,4 @@
-
+<main>
 <nav class="navbar navbar-expand-lg navbar-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -15,10 +15,18 @@
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Pre Order</a>
       </li>
+       <?php if(isset($this->session->userdata['logged_in'])){?>
       <li class="nav-item">
-        <?php if(isset($this->session->userdata['logged_in'])){?>
+       
 
         <a class="nav-link" href=""><?= $this->session->userdata['logged_in'] ?></a>
+      </li>
+       <li class="nav-item">
+       
+
+        <a class="nav-link" href="<?php echo site_url('Login/do_logout') ?>">logout</a>
+      </li>
+       <li class="nav-item">
       <?php }else{ ?>
         <a class="nav-link" href="<?php echo site_url('Login/index') ?>">Login</a>
 
